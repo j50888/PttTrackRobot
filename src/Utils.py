0eec0261ca8ddc17724ae6123f7a1453b5eee857
j50888 <j50888@hotmail.com>
2018-09-06 22:42:56 +0800

    Remove redundant function

--- src/Utils.py
+++ src/Utils.py
@@ -6,14 +6,6 @@ from pprint import pprint
 DEBUG = False
 
 
-def TrackGame(articles):
-    for obj in articles:
-        title = obj['article_title']
-        print('Processing ' + title + '...')
-        if 'NS' in title and '奧德賽' in title:
-            print(obj['content'])
-
-
 def ParseJson(filePath=os.path.join(os.path.dirname(__file__), '../config.json')):
     print('Parsing config.json...')
 
